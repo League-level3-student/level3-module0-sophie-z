e97de1e2b90109d7bb48931f6b369b462ba3c7b5
sophie-z <sophie.zeng2@gmail.com>
2020-10-06 19:55:12 -0700

    s

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,7 +4,10 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
+		int[] integers = new int[5];
+		for(int i = 0; i <= integers.length; i++){ 
+		    
+		}
 		return 0;
 	}
 	
